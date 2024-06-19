1523a408d61ac1abdc2a2a31066b6bb367092cfb
Divyanshu Rana <141987294+Ranavanshi@users.noreply.github.com>
2024-06-19 20:26:42 +0530

    Umm

--- AnonXMusic/plugins/bot/start.py
+++ AnonXMusic/plugins/bot/start.py
@@ -102,7 +102,7 @@ async def start_gp(client, message: Message, _):
     out = start_panel(_)
     uptime = int(time.time() - _boot_)
     await message.reply_photo(
-        photo=config.START_IMG_URL,
+        photo=config.DEAD_IMG_URL,
         caption=_["start_1"].format(app.mention, get_readable_time(uptime)),
         reply_markup=InlineKeyboardMarkup(out),
     )
